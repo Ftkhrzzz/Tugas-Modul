d2133cef00943de3fd9807ff1e22b8a2663eb6f2
Revan Fatkhurezi <161547483+Ftkhrzzz@users.noreply.github.com>
2024-06-02 21:13:03 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from hitung_huruf_besar import hitung_huruf_besar
+
+teks = "Hallo, Perkenalkan nama saya Revan Fatkhurezi Desviansyah"
+print(hitung_huruf_basar(teks))
+
